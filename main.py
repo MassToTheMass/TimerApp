669ee855dfe8829cb9004e654622db7ab71cd983
= <=>
2025-01-15 11:33:25 -0700

    start menu bar

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import tkinter as tk
-
-
+from menu import *
 
 root = tk.Tk()
+
+root.geometry("500x300")
+
+menu = tk.Menu(root)
+configureMenu(menu)
+root.config(menu=menu)
+
+
+
+root.mainloop()
