d1695106523d2d4aca55e926bfd70c1d43622ccc
Mason Christopherson <masonxiv.14@icloud.com>
2025-01-17 02:16:01 +0000

    make only time function

--- main.py
+++ main.py
@@ -3,14 +3,5 @@ from menu import *
 import datetime
 from time_funcs import getTime
 
-root = tk.Tk()
-
-root.geometry("500x300")
-
-menu = tk.Menu(root)
-configureMenu(menu)
-root.config(menu=menu)
-
-
-root.after(1000, print(getTime))
-root.mainloop()
+for i in range(500):
+    print(getTime())
--- time_funcs.py
+++ time_funcs.py
@@ -1,6 +1,6 @@
 import datetime
 
-def getTime():
-    current_time = datetime.datetime.now()
+def getSystemTime():
+    current_time = datetime.datetime.now().time()
 
     return current_time
